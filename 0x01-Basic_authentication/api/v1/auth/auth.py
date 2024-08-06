ec4a65b7f0ef6a5a4391a719b159bb5c54896a5e
Omar-Algassim <omarggg2@gamil.com>
2024-08-06 17:58:20 +0300

    implement authorization_header method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,7 @@ from typing import List, TypeVar
 class Auth:
     """authintication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """return Fasle"""
+        """return Fasle if path in execluded_paths true otherwise"""
         if not path:
             return True
         if type(path) == str and not path[-1] == '/':
@@ -20,7 +20,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """return None - request will be flask request object"""
-        return None
+        if not request or not request.headers.get('Authorization'):
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """return None - request will be flask request object"""
