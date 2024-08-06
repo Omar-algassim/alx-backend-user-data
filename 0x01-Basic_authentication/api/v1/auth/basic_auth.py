5f03ffda67c9f12a618f54bc0aa84d88dfc1c8c7
Omar-Algassim <omarggg2@gamil.com>
2024-08-06 19:46:10 +0300

    add documentation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """basic auth class"""
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """extract base64 authorization header"""
         if not authorization_header or not isinstance(authorization_header, str):
             return None
         if not authorization_header.startswith("Basic "):
