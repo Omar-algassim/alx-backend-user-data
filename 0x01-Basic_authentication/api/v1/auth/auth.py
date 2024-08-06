bf1c1fd281402cf1efa0d52835c164e041aa21ba
Omar-Algassim <omarggg2@gamil.com>
2024-08-06 16:20:29 +0300

    add documentation and pycodestyle fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+"""authintication class"""
 from flask import request
 from typing import List, TypeVar
 
+
 class auth:
     """authintication class"""
-    def  require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """return Fasle"""
         return False
 
