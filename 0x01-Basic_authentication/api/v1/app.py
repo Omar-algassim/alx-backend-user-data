c6a1cf8cb62bc6313881644a598173020a0f5e59
Omar-Algassim <omarggg2@gamil.com>
2024-08-06 18:00:20 +0300

    pycodestyle fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -44,7 +44,11 @@ def check_auth() -> None:
     """filter request want autherization or not"""
     if not auth:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+        ]
     if not auth.require_auth(request.path, excluded_paths):
         return
     if not auth.authorization_header(request):
