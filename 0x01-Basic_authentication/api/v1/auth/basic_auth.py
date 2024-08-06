3a2328c88ab4d64525c26cdb70165ee2f687069d
Omar-Algassim <omarggg2@gamil.com>
2024-08-06 22:02:29 +0300

    implement decode_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """basic auth class"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -12,3 +13,12 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """decode base64 authorization header"""
+        if not base64_authorization_header or not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
