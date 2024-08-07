52b6b6ceadf2c1d3b6f8d817fa3fc2ba96d06a7b
Omar-Algassim <omarggg2@gamil.com>
2024-08-08 01:54:22 +0300

    impelemnt session cookies method

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """authintication class"""
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -35,3 +36,9 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """return None - request will be flask request object"""
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+        return request.cookies.get(os.getenv('SESSION_NAME'))
