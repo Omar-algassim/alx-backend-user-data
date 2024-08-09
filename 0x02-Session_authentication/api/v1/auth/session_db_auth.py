5ea6c4f565d29fe28cf27d6edca5d1d5d88edae2
Omar-Algassim <omarggg2@gamil.com>
2024-08-09 05:01:09 +0300

    session DB Auth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+""" UserSession module
+"""
+from api.v1.auth.session_exp_auth import SessionExpAuth
+
+
+class SessionDBAuth(SessionExpAuth):
+    """SessionDBAuth class"""
+    
+    def def create_session(self, user_id=None):
+        """create session"""
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+        UserSession(user_id=user_id, session_id=session_id).save()
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """get user id for session id"""
+        if session_id is None:
+            return None
+        user_session = UserSession.search({'session_id': session_id})
+        if not user_session:
+            return None
+        if self.session_duration <= 0:
+            return user_session[0].user_id
+        if 'created_at' not in user_session[0].__dict__:
+            return None
+        if (
+            datetime.now() - user_session[0].created_at
+        ).seconds > self.session_duration:
+            return None
+        return user_session[0].user_id
+
+    def destroy_session(self, request=None):
+        """destroy session"""
+        if request is None:
+            return False
+        session = self.session_cookie(request)
+        if session is None:
+            return False
+        user_session = UserSession.search({'session_id': session})
+        if not user_session:
+            return False
+        user_session[0].remove()
+        return True
