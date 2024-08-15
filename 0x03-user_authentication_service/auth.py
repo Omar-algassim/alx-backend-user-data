90255b25a39c707d356da9202ec3163de8968460
Omar-Algassim <omarggg2@gamil.com>
2024-08-15 18:49:06 +0300

    pycodestyle fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,9 +18,11 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def _generate_uuid() -> str:
     """return a uuid string"""
     return str(uuid.uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
