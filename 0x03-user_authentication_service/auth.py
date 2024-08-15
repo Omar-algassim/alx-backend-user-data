272caef410c3acaf40d05156601ad9b12029637b
Omar-Algassim <omarggg2@gamil.com>
2024-08-15 14:42:29 +0300

    upload the hsh method only

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,31 +1,13 @@
 #!/usr/bin/env python3
+"""auth module"""
 from bcrypt import hashpw, gensalt
 from user import User
 from sqlalchemy.exc import NoResultFound
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
     """hash a password
     """
     salt = gensalt()
-    hash_pw = hashpw(password.encode('utf-8'), salt)
-    return hash_pw
-
-from db import DB
-
-
-class Auth:
-    """Auth class to interact with the authentication database.
-    """
-
-    def __init__(self):
-        self._db = DB()
-
-    def register_user(self, email: str, password: str) -> User:
-        """register a user"""
-        try:
-            self._db.find_user_by(email=email)
-            raise ValueError("User {} already exists".format(email))
-        except NoResultFound:
-            return self._db.add_user(email, _hash_password(password))
-
+    return hashpw(password.encode('utf-8'), salt)
