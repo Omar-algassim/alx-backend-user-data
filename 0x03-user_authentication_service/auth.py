abf2f74ce5ce80bf64dc7b9d41eb5c751b8e1ff2
Omar-Algassim <omarggg2@gamil.com>
2024-08-15 21:01:20 +0300

    fix comma in update user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -96,7 +96,7 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             hased_password = _hash_password(password)
             self._db.update_user(user.id,
-                                 hashed_password=hased_password
+                                 hashed_password=hased_password,
                                  reset_token=None
                                  )
         except NoResultFound:
