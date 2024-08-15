43e9b9d27ca7e1c218bffe644dd38581489a21f9
Omar-Algassim <omarggg2@gamil.com>
2024-08-15 15:45:40 +0300

    add utf-8 to encode

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """auth module"""
 from bcrypt import hashpw, gensalt
-from user import User
-from sqlalchemy.exc import NoResultFound
-from db import DB
 
 
 def _hash_password(password: str) -> bytes:
-    """hash a password
+    """return hashed password
+
+    Args:
+        password (str): passwword string
+
+    Returns:
+        bytes: the bytes returned from hashpw
     """
-    return hashpw(password.encode(), gensalt())
+    return hashpw(password.encode('utf-8'), gensalt())
