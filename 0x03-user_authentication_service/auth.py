54d13eb4cfacd100743cc83543cc92af8eec7f14
Omar-Algassim <omarggg2@gamil.com>
2024-08-15 18:56:16 +0300

    implement destroy session and get user from session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,3 +64,18 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """get user from session id"""
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """destroy session after logout"""
+        self._db.update_user(user_id, session_id=None)
+        return None
